d1341f3271d0e347c25283528d0b6b029fd33db5
josiearcuri <Josephine.Arcuri@colorado.edu>
2025-11-19 09:54:13 -0700

    update file names and remove old outputs

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from IPython.display import Image
 
-from src import permabankerosionmodel, boundaryconditionstools, watertemperaturemodel
+from src import permafrostbankerosionmodel as pbem
+from src import boundaryconditionstools as bct
+from src import watertemperaturemodel as wtm
 
 def get_params():
     """
@@ -31,7 +33,7 @@ def get_params():
     # Spacing and timing
     options['dt'] = 60*60*3
     options['dz'] = 0.01
-    options['runtime'] =60*60*24*365*2
+    options['runtime'] =60*60*24*365*5
 
     ##### River Ice Conditions #####
 
@@ -53,22 +55,22 @@ def get_params():
     options['D50bank'] = 50e-6
 
     #height of river bank exposed above gravel bank toe
-    options['bh']  = 1.7
+    options['bh']  = 2
 
     #gravel thickness on river bank toe above the river bed
     options['gravelz'] = .5
 
     #bankfull river bank height - smallest along the cross section runnign perpendicular to the bank
-    options['bfbh']  =   1.6
+    options['bfbh']  =   2
 
     #bankfull channel wetted perimeter, width at high stage
-    options['width']  = 790
+    options['width']  = 500
 
     #ice wedge spacing / distance between troughs on surface, thiis sets the overhanging block dimensions, and how often collapses occur
-    options['Xiw'] = 35
+    options['Xiw'] = 10
 
     #river bank ice content, in percent of volume, ranging between 40 and 100 %
-    options['Fi'] = 75
+    options['Fi'] = 80
 
     #starting river water depth. values below 'starth' are set equal to 'starth', because the river water temperature model is innacurate when flow is shallow. 
     options['starth'] = options['gravelz'] 
@@ -98,25 +100,24 @@ def run_model_scenario(params):
     """
         
     # define wiver water stage and meteorological boundary conditions in a dictionary
-    BCT= boundaryconditionstools.BoundaryConditionsTools(params)
-    bcdict = BCT.get_bcs()
-
-    bcdict= watertemperaturemodel.River_temp_model(bcdict)
+    bcdict= bct.BoundaryConditionsTools(params).get_bcs()
+    bcdict= wtm.River_temp_model(bcdict)
     
     # run bank erosion model
-    BEM = permabankerosionmodel.BankErosionModel(params, bcdict)
-    BEM.run()
-    erosionrate = BEM.get_erosion()
+    model = pbem.PermafrostBankErosionModel(params, bcdict)
+    model.run()
+    erosionrate = model.get_erosion()
     resultsdict = {"meanannualerosionrate_mperyr":erosionrate}
     
-    #add parameters and results to pandas dataframe
+    # add parameters and results to pandas dataframe
     df = pd.DataFrame(params|resultsdict, index = [0])
     
-    #print and plot results
-    fig = BEM.plot_bankprofile(title = str("E = " + str(np.round(erosionrate,2))+ " meters/year"))
+    # plot results
+    fig = model.plot_bankprofile(title = str("E = " + str(np.round(erosionrate,2))+ " m/yr"))
     plt.savefig("outputs/"+str(params["identifier"])+"_finalprofile.jpg", dpi = 300)
     plt.close()
     return df
+
 #define parameters
 params = get_params()
 
